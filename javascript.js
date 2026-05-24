c1382d6ab492ed9cfc2cc3330ba70373a9d5f13f
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-05-24 09:25:22 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "sudhanshuzoom1@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+ // ccountId = 3 // not allowed
+
+accountEmail = "helk.com"
+accountPassword = "543543"
+accountCity = "bangalore"
+
+console.log([accountId , accountEmail , accountPassword , accountCity ]);
+// prefer not to use var because of issue in blockscope and functionals scope
+
+
+
