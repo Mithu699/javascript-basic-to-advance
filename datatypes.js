73c1b1f5dbff5336244c2265342bf3efe33cf14e
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-05-24 10:49:30 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello ") // we are using node js not browser
+
+console.log(3 + 3); // code read krne layak ho 
+console.log("sudhanshu");
+
+let name = "sudhanshu"
+let age = 19
+let isloggedIn = false
+let state;
+
+// number => 2 to power 32
+// bigInt => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined =>
+// symbol => unique 
+
+//. these all are primitive data types
+
+console.log(typeof "sudhanshu");
+console.log(typeof age);
+console.log(typeof null);
