924e634bae1fbc6987ea86f99d149e9ba339f6e3
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-05-25 11:09:55 +0530

    Add files via upload

---
+++ operationindt.js
@@ -0,0 +1,65 @@
+let score = "33" // terminal me NaN aayega jiska mtlb hota hai not a number 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+ // console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 and false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "sudhanshu" => true 
+
+let someNumber = 33
+ 
+let stringNumber = String(someNumber)
+// console.log (stringNumber);
+// console.log (typeof stringNumber);
+
+// *********************************    OPERATIONS    ***************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " sudhanshu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "3")
+// console.log("1" + 3)
+// console.log(1 + 3)
+// console.log((1 + 3) * 5 % 3)
+ // console.log(true)
+
+
+ let num1, num2, num3
+
+ num1 = num2 = num3 = 3+3
+ 
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+
+  
