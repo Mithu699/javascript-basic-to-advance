2def0306776490e902cb6ce6d92a5fc62f806bcd
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-05-25 11:33:19 +0530

    Add files via upload

---
+++ comparisonindatatypes.js
@@ -0,0 +1,16 @@
+console.log("2" > 1);
+console.log("33" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// inn tarah ke comparison or operation kabhi bhi confusion me daal skte hai to inka use na hi kre ya bahut zyada kam kre
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+ 
+// ===
+
+console.log("2" === 2);
