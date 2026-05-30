a3a35cc5ce9edafeb5ccc50ae28b88a168cf4eb5
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-05-30 16:31:47 +0530

    Add files via upload

---
+++ objects3.js
@@ -0,0 +1,29 @@
+// objects part 3 
+const course = {
+  coursename: "js in hindi",
+  price : "999",
+  courseteacher : "hitesh"
+
+}
+
+// console.log(course.courseteacher) // ye acha hai koi galti nahi h pr conventional nahi h  utna 
+
+const{price} = course // issi ko kehte destructuring in javascript useful in react too
+console.log(price); // ye wala use krnge zyada behtar hoga 
+
+
+// API *********************************************************************************
+
+/*
+{
+  "name": "sudhanshu",
+coursename: "js in hindi", 
+price: "free"
+} */
+
+[
+  {},
+  {},
+  {},
+  
+]
