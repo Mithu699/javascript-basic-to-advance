6360cb15d0e24a4918433bf4618885ffe7edee92
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-06-06 22:54:40 +0530

    Add files via upload

---
+++ objectloops.js
@@ -0,0 +1,36 @@
+const myObject = {
+    js : 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by app"
+}
+
+
+// for in loop
+
+for (const key  in myObject) {
+ //  console.log(myObject[key]);
+   
+}
+
+const programming = ["js","rb","py","java","cpp"]
+for (const key in programming) {
+   //  console.log(programming[key]);
+}
+
+/*
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United states of america");
+map.set("Fr", "france");
+map.set("IN", "India");
+
+for (const key in map) {
+console.log(key);
+
+} */
+
+
+
+
+
